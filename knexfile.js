6abea53469dc10d000abb1a6f6503e40f1241835
Kyriakos Bakalis <greekgodsocrates.com>
2024-01-17 17:31:15 -0500

    .env and knex.js updated

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
 module.exports = {
   client: 'mysql',
   connection: {
-    database: process.env.DB_LOCAL_DBNAME,
-    user:     process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   },
   migrations: {
     tableName: 'knex_migrations'

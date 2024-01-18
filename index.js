a9fa07110713c1097bef56eb99615ffc1e651fa0
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2024-01-18 12:09:36 -0500

    Add basic server and example route and controller

---
+++ index.js
@@ -0,0 +1,34 @@
+// Clear and set up new run
+console.clear();
+console.log("New run-------------------------------------------------------------------------------------");
+
+// Import dependencies
+require('dotenv').config();
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5050;
+
+// Configure our server
+app.use(cors());
+app.use(express.json());
+
+
+// Print to console whenever anyone hits any endpoint on the server
+app.use((req, _res, next) => {
+    const moment = new Date(Date.now()) 
+    console.log("hit for", req.originalUrl, "at", moment.toLocaleString('en-US', { timeZone: 'America/New_York' }));
+    next();
+})
+
+// Basic response for home page
+app.get("/", (_req, res) => {
+    res.send("you've hit the home page. this means you can access our server =)");
+})
+
+// Set up routes for inventories
+const inventoryRoutes = require("./routes/inventory-routes");
+app.use("/inventory", inventoryRoutes);
+
+// Spin up server
+app.listen(PORT, () => console.log(`running at http://localhost:${PORT}`));
---
+++ routes/inventory-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const inventoryController = require('../controllers/inventory-controller');
+
+router
+    .route("/")
+    .get(inventoryController.index)
+    // .get((_req, res) => res.send("you've hit the /inventory page"))
+    // .post(userController.add);
+
+// router
+//     .route("/:id")
+//     .get(userController.findOne)
+//     .patch(userController.update)
+//     .delete(userController.remove);
+
+// router
+//     .route("/:id/posts")
+//     .get(userController.posts);
+
+module.exports = router;

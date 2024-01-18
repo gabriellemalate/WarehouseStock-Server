6f8528eaef59c4db2a18d214295b2c18d4f0f739
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2024-01-18 12:43:07 -0500

    Get all necessary info from database for /inventory endpoint

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -8,10 +8,10 @@ const index = async (_req, res) => {
     try {
         const data = await knex("inventories")
             .join("warehouses", "warehouse_id", "warehouses.id")
-            .select("inventories.id", "warehouse_name");
+            .select("inventories.id", "item_name", "category", "status", "quantity", "warehouse_name");
         res.status(200).json(data);
     } catch (error) {
-        res.status(400).send(`Error retrieving Inventories: ${error}`);
+        res.status(400).send(`Error retrieving from Inventories: ${error}`);
     }
 };
 

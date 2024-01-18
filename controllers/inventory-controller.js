a26b80fe9c9eff78c6b295f84e3752fe65dca362
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2024-01-18 12:36:15 -0500

    Test getting some info from database for /inventory endpoint

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,7 +1,18 @@
 const knex = require("knex")(require("../knexfile"));
 
+// const index = async (_req, res) => {
+//     res.send("you've hit the /inventory page! there's nothing here yet")
+// };
+
 const index = async (_req, res) => {
-    res.send("you've hit the /inventory page! there's nothing here yet")
+    try {
+        const data = await knex("inventories")
+            .join("warehouses", "warehouse_id", "warehouses.id")
+            .select("inventories.id", "warehouse_name");
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).send(`Error retrieving Inventories: ${error}`);
+    }
 };
 
 module.exports = {

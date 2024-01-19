22e5c6bb69f6722f8b27014df1336129b1983c89
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-19 10:58:29 -0500

    Create base endpoint from createWarehouse endpoint

--- controllers/warehouses-controller.js
+++ controllers/warehouses-controller.js
@@ -17,7 +17,6 @@ const createWarehouse = async (req, res) => {
     }
 
     try {
-        // const result = await knex("user").insert(req.body);
         const result = await knex('warehouses').insert(req.body);
         
         const newUserId = result[0];
@@ -36,6 +35,43 @@ const createWarehouse = async (req, res) => {
     }
 }
 
+const updateWarehouse = async (req, res) => {
+    let {warehouse_name, address, city, country, contact_name, contact_position, contact_phone, contact_email} =  req.body;
+
+    if( !warehouse_name || !address || !city || !country || !contact_name || !contact_position || !contact_phone || !contact_email) {
+        res.status(400).json({
+            message: "Please provide warehouse name, street address, city, country, warehouse contact name, contact position, contact phone number, and contact email in the request"
+        })
+    }
+
+    if (!emailIsValid(contact_email) || !phoneNumberIsValid(contact_phone)) {
+        res.status(400).send({
+            message: "Please provide contact email in the format 'yourname@example.com' and contact phone number in the format in the format '+1 (234) 567-8910'"
+        })
+    }
+
+    try {
+        const result = await knex('warehouses')
+            .where({ id: req.params.id })
+            .update(req.body);
+        
+        const newUserId = result[0];
+
+        const createdWarehouse = await knex
+            .select("id", "warehouse_name", "address", "city", "country", "contact_name", "contact_position", "contact_email")
+            .from("warehouses")
+            .where({ id: newUserId});
+
+        res.status(201).json(createdWarehouse);
+
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to update warehouse: ${error}`
+        })
+    }
+}
+
 module.exports = {
-    createWarehouse
+    createWarehouse,
+    updateWarehouse,
 };

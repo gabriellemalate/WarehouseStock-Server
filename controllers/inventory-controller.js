0bc8f1ab3a5d67da11863e38ca6914a5f4d0c947
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-20 11:11:48 -0500

    Create item creation controller

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -12,7 +12,37 @@ const index = async (_req, res) => {
 };
 
 const createItem = async (req, res) =>  {
+    let { warehouse_id, item_name, description, category, status, quantity } = req.body;
 
+    if (!warehouse_id || !item_name || !description || !category || !status || !quantity) {
+        res.status(400).json({
+            message: "Please provide item name, item description, category, status, quantity, and warehouse id in the request"
+        })
+    }
+
+    if (isNaN(quantity)) {
+        res.status(400).send({
+            message: "Please provide a numerical quantity"
+        })
+    }
+
+    try {
+        const result = await knex('inventory').insert(req.body);
+
+        const newInventoryId = result[0];
+
+        const createdInventoryItem = await knex
+            .select("id", "item_name", "description", "category", "status", "quantity", "warehouse_id")
+            .from("inventory")
+            .where({ id: newInventoryId });
+
+        res.status(201).json(createdInventoryItem);
+
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to create new inventory item: ${error}`
+        })
+    }
 }
 
 module.exports = {
